73779e9ecdb31ae60d449a219a25f7901db0861d
Nguyen Tuan Vu <vudeeptryz123@gmail.com>
2025-12-16 09:48:20 +0700

    Update: main.py - Final version with CaroGUI, CaroBoard, and minimax imports

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
 # main.py
 from gui.caro_gui import CaroGUI
 from game.board import CaroBoard
-from ai.ai_caro import CaroAI  # Import đúng class và tên file
+from ai.minimax import find_best_move, X, O, EMPTY   # nếu cần
 
 if __name__ == "__main__":
-    board = CaroBoard()        
-    ai = CaroAI()              
-    game = CaroGUI(board_size=15, board=board, ai=ai)
-    game.run()
+    app = CaroGUI()
+    app.run()
